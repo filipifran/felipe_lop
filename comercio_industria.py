760609b182a6b815994c85f9a0e8da23d7058cca
filipifran <felipecarter77@gmail.com>
2026-03-30 08:53:05 -0300

    Add files via upload

---
+++ comercio_industria.py
@@ -0,0 +1,14 @@
+# Programa para calcular conta de água - comércio/indústria
+
+consumo = float(input("Digite o consumo de água (m3): "))
+
+if consumo <= 10:
+    valor = 44.95
+elif consumo <= 20:
+    valor = consumo * 8.75
+elif consumo <= 50:
+    valor = consumo * 16.76
+else:
+    valor = consumo * 17.46
+
+print(f"Valor da conta: R$ {valor:.2f}")
